15f99021aa00416eb6c6bc58d5325b258d6afe39
Zheng Ma <mazhengcn@outlook.com>
2022-05-15 02:01:23 +0800

    🦄 refactor: add bash script
    add bash script for inference

---
+++ inference_tool/inference.sh
@@ -0,0 +1,5 @@
+
+export CUDA_VISIBLE_DEVICES="0"
+
+python inference.py --config_path=/home/mazheng/waste-detection/Yet-Another-EfficientDet-Pytorch/inference_tool/config/config.yml
+# python inference_video.py --config_path=/home/mazheng/waste-detection/Yet-Another-EfficientDet-Pytorch/inference_tool/config/config_video.yml
